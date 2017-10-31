06f73b84c982f2c052131d80e154a09ea2aa44e9
Siddhesh <isiddhesh45@gmail.com>
2017-10-31 01:05:28 -0400

    #14 Jenkins

--- HelloWorld.py
+++ HelloWorld.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Presto Magico!! :) :) :) :)"
+    return "Presto Magico!! :D :D :D"
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
