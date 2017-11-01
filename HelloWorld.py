972c7a30ff807c58dd2fe90e790137be930cbc18
Zishan Ahmad <zahmad@teladoc.com>
2017-11-01 10:49:35 -0400

    Jenkins, come on now...

--- HelloWorld.py
+++ HelloWorld.py
@@ -5,8 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-
-    return "You are gonna edit me :( Pipeline works fine though! Check it one last time"
+    return "Make it so!"
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
