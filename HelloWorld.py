4a7861f26a3aa270dcf48862a1aa0fe7c1d4d8a1
Siddhesh Shahu Salunke <siddhesh.salunke45@gmail.com>
2017-11-01 07:48:13 -0400

    Update HelloWorld.py

--- HelloWorld.py
+++ HelloWorld.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
 
-    return "You aregonna edit me :("
+    return "You are gonna edit me :("
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
