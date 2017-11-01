ed56e0152b2b08bdefbbb1828fe203c348c14e33
Zishan Ahmad <github@zishanahmad.com>
2017-11-01 10:52:25 -0400

    roll of the dice

--- HelloWorld.py
+++ HelloWorld.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Make it so!"
+    return "Make it so! Please."
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
