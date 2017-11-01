2e16f297bda30193001b7e94ffc167b868d896f8
Siddhesh Shahu Salunke <siddhesh.salunke45@gmail.com>
2017-11-01 12:04:53 -0400

    okay

--- HelloWorld.py
+++ HelloWorld.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Make it so! Please."
+    return "Make it so! Yes Please."
 
 if __name__ == "__main__":
     app.run(debug=True, host='0.0.0.0')
